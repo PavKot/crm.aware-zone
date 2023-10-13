db840ae26478594b76dd84bbb0880e01816073bb
PavKot <pavlo.melnik.20043@gmail.com>
2023-10-13 15:55:59 +0300

    Update EventsCalendar.jsx

--- src/Components/EventsCalendar/EventsCalendar.jsx
+++ src/Components/EventsCalendar/EventsCalendar.jsx
@@ -18,37 +18,44 @@ const EventsCalendar = () => {
   }, []);
   return (
     /* beautiful calendar of events for users */
-    <div className="events-calendar">
-      <h1>Календар подій</h1>
-      <div className="events-calendar-content">
-        <div className="events-calendar-content-header">
-          <div className="events-calendar-content-header-date">
-            <h2>Дата</h2>
-          </div>
-          <div className="events-calendar-content-header-place">
-            <h2>Місце</h2>
-          </div>
-          <div className="events-calendar-content-header-details">
-            <h2>Деталі</h2>
-          </div>
-        </div>
-        <div className="events-calendar-content-events">
-          {events.map((event) => (
-            <div className="events-calendar-content-event">
-              <div className="events-calendar-content-event-date">
-                <p>{event.date}</p>
-              </div>
-              <div className="events-calendar-content-event-place">
-                <p>{event.city}</p>
-              </div>
-              <div className="events-calendar-content-event-details">
-                <p>{event.name}</p>
-              </div>
+    <>
+      <div className="events-calendar">
+        <h1>Календар подій</h1>
+        <div className="events-calendar-content">
+          <div className="events-calendar-content-header">
+            <div className="events-calendar-content-header-date">
+              <h2>Дата</h2>
             </div>
-          ))}
+            <div className="events-calendar-content-header-place">
+              <h2>Місце</h2>
+            </div>
+            <div className="events-calendar-content-header-details">
+              <h2>Деталі</h2>
+            </div>
+          </div>
+          <div className="events-calendar-content-events">
+            {events.map((event) => (
+              <div className="events-calendar-content-event">
+                <div className="events-calendar-content-event-date">
+                  <p>{event.date}</p>
+                </div>
+                <div className="events-calendar-content-event-place">
+                  <p>{event.city}</p>
+                </div>
+                <div className="events-calendar-content-event-details">
+                  <p>{event.name}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      <footer className="record-footer">
+        <a href="https://aware-zone.com/">
+          Повернутись на сайт <span>aWARe Zone</span>
+        </a>
+      </footer>
+    </>
   );
 };
 
